7496796fdf9a961eabafbbc8095cf37f09be1856
Ilmir <code.for.func@gmail.com>
2023-09-10 15:28:04 +0400

    Add index method for admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,16 @@
 
   class Admin::UsersController < Admin::ApplicationController
+
+    def index
+      @q  = User.ransack(params[:q])
+      @users = @q
+                .result
+                .order(:type)
+                .order(:id)
+                .page(params[:page])
+                .per(params[:per_page])
+    end
+
     def show
       @user = User.find(params[:id])
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["avatar", "created_at", "email", "first_name", "id", "last_name", "password_digest", "type", "updated_at"]
+  end
 end
